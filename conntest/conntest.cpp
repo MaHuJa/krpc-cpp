073ff369434ff390d2b41f5f9fa4c33fb5cd3fc4
MaHuJa <github@mahuja.net>
2014-06-03 13:34:45 +0200

    Changed to asio stream form

--- conntest/conntest.cpp
+++ conntest/conntest.cpp
@@ -9,19 +9,20 @@ int main(int argc, char** argv)
 	asio::io_service ioservice;
 	asio::ip::tcp::socket socket(ioservice);
 	try {
-		auto addr = asio::ip::address::from_string("127.0.0.1");
-		asio::ip::tcp::endpoint remote_endpoint(addr, 50000);
-		socket.connect(remote_endpoint);
-
-		if (!socket.is_open()) {
+		asio::ip::tcp::iostream iostr("127.0.0.1", "50000");
+		if (!iostr) {
 			std::cout << "Failed to open\n";
 			return 1;
 		}
-		auto i_am_krpc_client = asio::buffer("\x48\x45\x4C\x4C\x4F\xBA\xDA\x55", 8);
-		asio::write(socket, i_am_krpc_client);
+		iostr << "\x48\x45\x4C\x4C\x4F\xBA\xDA\x55";
 		{	char namebuf[32] = "krpc-cpp-mhj";
-		asio::write(socket, asio::buffer(namebuf, 32));
+			iostr.write(namebuf, 32);
 		}
+
+
+
+
+
 
 		return 0;
 	}
